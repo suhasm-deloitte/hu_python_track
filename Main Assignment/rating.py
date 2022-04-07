d23849233b8dfe9a0eb41bef79c9d4aebf2c8b0b
suhasm-deloitte <suhasm@deloitte.com>
2022-04-08 01:06:15 +0530

    added ratings functionality

---
+++ Main Assignment/rating.py
@@ -0,0 +1,15 @@
+def ratings():
+    movies = ["RRR", "KGF", "HIT"]
+    for movie in movies:
+        print("Rating For Movie: ", movie)
+
+        for i in range(len(movies)):
+            rating = int(input("Enter Movie rating: "))
+            if rating < 1 or rating > 5:
+                print("That's not a Valid number!")
+                continue
+            break
+        print("*" * rating)
+
+
+ratings()
